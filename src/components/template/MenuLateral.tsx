d1a3515e2b33c76277fe876240ebc34990c3f903
PauloMtz <pramosmtz@gmail.com>
2022-05-08 16:49:48 -0300

    Componente Menu Lateral #1

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,7 +1,14 @@
+import { IconeAjustes, IconeHome, IconeSino } from "../icones";
+import MenuItem from "./MenuItem";
+
 export default function MenuLateral() {
     return (
         <aside>
-            <h2>Menu Lateral</h2>
+            <ul>
+                <MenuItem url="/" texto="Início" icone={IconeHome} />
+                <MenuItem url="/configuracoes" texto="Configurações" icone={IconeAjustes} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
         </aside>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: [
+  purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {}
   },
   plugins: [],
 }
