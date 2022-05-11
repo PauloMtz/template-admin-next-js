7cbc500869b22ca76e6ec1260e2690c8a0b06ce1
PauloMtz <pramosmtz@gmail.com>
2022-05-11 19:48:04 -0300

    Utilizando Context Api #1 - Consumer

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,12 @@
 import Layout from "../components/template/Layout";
+import { AppConsumer } from "../data/context/AppContext";
 
 export default function Notificacoes() {
   return (
     <Layout titulo="Notificações" subtitulo="Página de Notificações">
-      <h3>Conteudo Notificações!!!</h3>
+      <AppConsumer>
+        {dados => <h3>{dados.nome}</h3>}
+      </AppConsumer>
     </Layout>
   )
 }
