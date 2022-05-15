e9d8a402045a112bd39ac6f4650762cc926d191c
PauloMtz <pramosmtz@gmail.com>
2022-05-15 09:18:04 -0300

    Autenticação #3 - Finalizando Formulário

--- src/components/icones/index.tsx
+++ src/components/icones/index.tsx
@@ -35,3 +35,9 @@ export const IconeLua = (tamanho = 6) => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
     </svg>
 )
+
+export const IconeAtencao = (tamanho = 6) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={`h-${tamanho} w-${tamanho}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
+    </svg>
+)
--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,27 +1,48 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { IconeAtencao } from "../components/icones";
 
 export default function Autenticacao() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [modo, setModo] = useState<'login' | 'cadastro'>('login')
+    const [erro, setErro] = useState(null)
+
+    function exibirMsgErro(msg, tempoSegundos = 5) {
+        setErro(msg)
+        setTimeout(() => setErro(null), tempoSegundos * 1000)
+    }
 
     function submeter() {
         if (modo === 'login') {
-            console.log("Modo Login")
+            exibirMsgErro("Ocorreu um erro ao efetuar login", 2)
         } else {
-            console.log("Modo Cadastro")
+            exibirMsgErro("Ocorreu um erro ao tentar cadastrar")
         }
     }
 
     return (
-        <div className="flex flex-col h-screen items-center justify-center">
-            <div className="w-1/2">
+        <div className="flex items-center justify-center">
+            <div className="hidden md:block w-1/2 lg:2/3">
+                <img src="https://source.unsplash.com/random" alt="Imagem"
+                    className="h-screen w-full object-cover" />
+            </div>
+            <div className="w-full md:w-1/2 m-10 lg:1/3">
                 <h1 className={`
-                    text-xl font-bold mb-5
+                    text-3xl font-bold mb-5
                 `}>
-                    {modo === 'login' ? 'Login' : 'Registre-se'}
+                    {modo === 'login' ? 'Efetuar Login' : 'Cadastre-se no site'}
                 </h1>
+
+                {erro ? (
+                    <div className={`
+                        flex items-center bg-red-400 text-white py-3 px-5
+                        border border-red-600 rounded-lg
+                    `}>
+                        {IconeAtencao()}
+                        <span className="ml-2">{erro}</span>
+                    </div>
+                ) : false}
 
                 <AuthInput label="E-mail" valor={email} tipo="email" 
                     valorMudou={setEmail} obrigatorio />
@@ -44,6 +65,23 @@ export default function Autenticacao() {
                 `}>
                     Entre com sua conta do Google
                 </button>
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Novo por aqui? 
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Cadastre-se</a> gratuitamente
+                    </p>
+                ) : (
+                    <p className="mt-8">
+                        Já sou ou cadastrado 
+                        <a onClick={() => setModo('login')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Efetuar Login</a>
+                    </p>
+                )}
             </div>
         </div>
     )
