68d6ee8435c5d91e4a8443db15d4c776bdb46358
PauloMtz <pramosmtz@gmail.com>
2022-05-15 17:04:34 -0300

    Autenticação com Google #3 - Avatar Usuário

---
+++ src/pages/perfil.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/template/Layout";
+
+export default function Perfil() {
+
+  return (
+    <Layout 
+      titulo="Perfil" 
+      subtitulo="Dados do usuário">
+      <h2>Alguma coisa sobre notificações</h2>
+    </Layout>
+  )
+}
